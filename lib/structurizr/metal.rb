09b76ac2637740f23fe3c8643c35eb4031c296bc
Igor S. Morozov <igor@morozov.is>
2020-07-08 01:50:48 +0300

    Expose view & client

--- lib/structurizr/metal.rb
+++ lib/structurizr/metal.rb
@@ -4,12 +4,15 @@ require 'java'
 
 require 'structurizr/metal/jars/commons-logging-1.2.jar'
 require 'structurizr/metal/jars/structurizr-core-1.4.7.jar'
+require 'structurizr/metal/jars/structurizr-client-1.4.7.jar'
 
 module Structurizr
   # This namespace provides lower-level Java integration with Structurizr
   module Metal
     Root = ::Java::ComStructurizr
     Model = ::Java::ComStructurizrModel
+    View = ::Java::ComStructurizrView
+    Client = ::Java::ComStructurizrClient
     Util = ::Java::ComStructurizrUtil
   end
 end
