ecdf1af8569c7bbf0bbe1f3d32f999e8ccc2aa89
Igor S. Morozov <igor@morozov.is>
2020-07-09 00:35:35 +0300

    Add simplest PaperSize

--- lib/structurizr.rb
+++ lib/structurizr.rb
@@ -4,6 +4,7 @@ require 'structurizr/version'
 require 'structurizr/workspace'
 require 'structurizr/interaction_style'
 require 'structurizr/location'
+require 'structurizr/paper_size'
 
 module Structurizr
   class Error < StandardError; end
---
+++ lib/structurizr/paper_size.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'structurizr/metal'
+
+module Structurizr
+  module PaperSize
+    def self.A5_Landscape # rubocop:disable Naming/MethodName
+      Metal::View::PaperSize::A5_Landscape
+    end
+  end
+end
