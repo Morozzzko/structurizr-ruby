ef92f2088573f4c93a31f7da2d872ca875eb3c41
Igor S. Morozov <igor@morozov.is>
2020-10-10 18:03:53 +0300

    Extract enums and documentation templates into a separate file (#3)

---
+++ lib/structurizr/documentation_templates.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'structurizr/structurizr_documentation_template'
+require 'structurizr/arc42_documentation_template'
+require 'structurizr/automatic_documentation_template'
+require 'structurizr/viewpoints_and_perspectives_documentation_template'
---
+++ lib/structurizr/enums.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'structurizr/interaction_style'
+require 'structurizr/location'
+require 'structurizr/paper_size'
+require 'structurizr/tags'
+require 'structurizr/shape'
+require 'structurizr/border'
+require 'structurizr/format'
+require 'structurizr/routing'
+require 'structurizr/filter_mode'
