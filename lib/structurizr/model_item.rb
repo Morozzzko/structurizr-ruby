9149ea6dc7c787a164b6db7f2690a62970b230b7
Igor S. Morozov <igor@morozov.is>
2022-06-10 18:45:52 +0300

    Add ModelItem class

---
+++ lib/structurizr/model_item.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'structurizr/metal'
+
+module Structurizr
+  ModelItem = Metal::Model::ModelItem
+
+  class ModelItem
+    def tags
+      get_tags.split(',')
+    end
+
+    def inspect
+      class_name = self.class.name.split('::').last
+      %(<#{class_name}: "#{name}">)
+    end
+  end
+end
